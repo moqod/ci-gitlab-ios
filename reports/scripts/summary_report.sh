cb8a7b3f5088f3b31986341c1cba53ace9b14575
Alexander Shilov <ashilov@moqod.com>
2017-06-22 18:20:23 +0400

    copy all summary files in ci_report folder

--- reports/scripts/summary_report.sh
+++ reports/scripts/summary_report.sh
@@ -36,12 +36,12 @@ JSON="{\"branch\":\"$BRANCH_NAME\",\
 \"bundle_version\":\"$BUNDLE_VERSION\",\
 \"reports\":[{\
 \"type\":\"clang\",\
-\"report_json\":\"/reports/clang/static/json/report.json\",\
+\"report_json\":\"/clang/json/report.json\",\
 \"report_html\":\"/clang/index.html\",\
 \"summary\":\"summary\"\
 },{\
 \"type\":\"jscpd\",\
-\"report_json\":\"/reports/jscpd/static/json/report.json\",\
+\"report_json\":\"/jscpd/json/report.json\",\
 \"report_html\":\"/jscpd/index.html\",\
 \"summary\":\"statistics\"\
 }]\
@@ -50,3 +50,6 @@ JSON="{\"branch\":\"$BRANCH_NAME\",\
 # echo $JSON
 
 python reports/scripts/summary.py $JSON
+
+cp -rf reports/index.html ci_report/
+cp -rf reports/static ci_report/
