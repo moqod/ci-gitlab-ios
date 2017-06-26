45858ed91a95d9d0218a7319eae6b1f6055b4cbd
Andrew Kopanev <andrew@moqod.com>
2017-06-26 11:06:10 +0400

    updated path

--- reports/scripts/summary_report.sh
+++ reports/scripts/summary_report.sh
@@ -36,12 +36,12 @@ JSON="{\"branch\":\"$BRANCH_NAME\",\
 \"bundle_version\":\"$BUNDLE_VERSION\",\
 \"reports\":[{\
 \"type\":\"clang\",\
-\"report_json\":\"ci_result/clang/report.json\",\
+\"report_json\":\"ci_result/clang/json/report.json\",\
 \"report_html\":\"clang/index.html\",\
 \"summary\":\"summary\"\
 },{\
 \"type\":\"jscpd\",\
-\"report_json\":\"ci_result/jscpd/report.json\",\
+\"report_json\":\"ci_result/jscpd/json/report.json\",\
 \"report_html\":\"jscpd/index.html\",\
 \"summary\":\"statistics\"\
 }]\
